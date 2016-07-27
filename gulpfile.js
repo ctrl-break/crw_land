185a9214ca3a38ced4ab2621f148630eee75e81d
fox <ctrl-break@mail.ru>
2016-07-27 17:10:25 +0400

    finish layout

--- gulpfile.js
+++ gulpfile.js
@@ -68,13 +68,13 @@ gulp.task('html', ['styles', 'scripts'], () => {
 
 gulp.task('images', () => {
   return gulp.src('app/images/**/*')
-    .pipe($.cache($.imagemin({
+    /*.pipe($.cache($.imagemin({
       progressive: true,
       interlaced: true,
       // don't remove IDs from SVGs, they are often used
       // as hooks for embedding and styling
       svgoPlugins: [{cleanupIDs: false}]
-    })))
+    })))*/
     .pipe(gulp.dest('dist/images'));
 });
 
